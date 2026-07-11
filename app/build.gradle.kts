c2cdb597e46933d3f0cce9fde58d515684be31d1
H4zh4n <hazhan@mail.com>
2026-07-11 17:45:27 +0200

    chore: bump version to 1.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.music.spotui"
         minSdk = 26
         targetSdk = 34
-        versionCode = 13
-        versionName = "1.3"
+        versionCode = 132
+        versionName = "1.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
